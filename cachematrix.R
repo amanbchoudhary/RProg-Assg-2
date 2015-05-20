dee6baa54f0ad00513645b235bc0041b332cf549
Aman Choudhary <500aman@gmail.com>
2015-05-21 00:31:58 +0530

    Initial Commit

---
+++ cachematrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) i <<- inv
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
